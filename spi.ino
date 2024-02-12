e9e8cb317c4d852e86121b05fced1a347c5698ef
rcs34 <119522267+rcs34@users.noreply.github.com>
2024-02-12 19:13:57 +0530

    Create spi.ino

---
+++ spi.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+
+const int latchPin1 = 8;
+const int latchPin2 = 9;
+const int dataPin1 = 11;
+const int dataPin2 = 10;
+const int dataPin3 = 13;
+
+byte blue1 = 0xFF;
+byte green1 = 0x00;
+byte blue2 = 0x00;
+byte green2 = 0xFF;
+byte red = 0xFF;
+
+void setup() {
+  SPI.begin();
+  pinMode(latchPin1, OUTPUT);
+  pinMode(latchPin2, OUTPUT);
+}
+
+void loop() {
+  setRed(red);
+  delay(1000);
+  setBlue(blue1, green1);
+  delay(1000);
+  setGreen(green2, blue2);
+  delay(1000);
+}
+
+void setRed(byte red) {
+  digitalWrite(latchPin2, LOW);
+  SPI.transfer(0x00); // Green
+  SPI.transfer(0x00); // Blue
+  digitalWrite(latchPin2, HIGH);
+
+  digitalWrite(latchPin1, LOW);
+  SPI.transfer(red); // Red
+  digitalWrite(latchPin1, HIGH);
+}
+
+void setBlue(byte blue, byte green) {
+  digitalWrite(latchPin1, LOW);
+  SPI.transfer(0x00); // Red
+  digitalWrite(latchPin1, HIGH);
+
+  digitalWrite(latchPin2, LOW);
+  SPI.transfer(blue); // Blue
+  SPI.transfer(green); // Green
+  digitalWrite(latchPin2, HIGH);
+}
+
+void setGreen(byte green, byte blue) {
+  digitalWrite(latchPin1, LOW);
+  SPI.transfer(0x00); // Red
+  digitalWrite(latchPin1, HIGH);
+
+  digitalWrite(latchPin2, LOW);
+  SPI.transfer(blue); // Blue
+  SPI.transfer(green); // Green
+  digitalWrite(latchPin2, HIGH);
+}
